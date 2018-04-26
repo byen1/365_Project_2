9845052ec37d92dd38327623e254e974f70dff56
austin saporito <austincsp@yahoo.com>
2018-04-26 15:51:02 -0400

    commented memory

--- Memory.java
+++ Memory.java
@@ -13,35 +13,46 @@ public class Memory {
     protected int sp;
     protected int ip;
     protected int size;
-    
+	/*
+		0 argument constructor
+	*/
     public Memory() {
         size = 1024;
         memory = new int[size];
         sp = size - 1;
         ip = 0;
     }
-    
+    /*
+		1 argument constructer to change size
+	*/
     public Memory(int sizeInBytes) {
         size = sizeInBytes/4;
         memory = new int[size];
         sp = size - 1;
         ip = 0;
     }
-    
+    /*
+		checks if pop will go out of bounds and returns the first value
+	*/
     public int pop() {
         if (sp + 1 >= size) throw new IndexOutOfBoundsException();
         return memory[sp++];
     }
-    
+    /*	
+		pushes the value passed to it on the stack
+	*/
     public void push(int val) {
         memory[--sp] = val;
     }
-    
+    /*
+		returns the instruction its on
+	*/
     public int getInstruction() {
         return memory[ip];
     }
-    
-    
+    /*
+		jumps to the label with the offset 
+	*/
     public void jump(int offset) {
         this.ip += (offset / 4);
     }
@@ -50,7 +61,9 @@ public class Memory {
         //load instructions from an array of instructions into memory starting at address 0
         System.arraycopy(instr, 0, memory, 0, instr.length);
     }
-
+	/*
+		returns the value of the stack with the offset and current stack pointer
+	*/
     public int peek(int offset)
     {
         if (sp + offset >= size) throw new IndexOutOfBoundsException();
